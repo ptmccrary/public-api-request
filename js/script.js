bf3b3bfeeb1fd22f71bfdb4f13fbeefbb87d2cc1
Parker McCrary <parkertomm3@gmail.com>
2020-08-20 23:37:47 -0400

    User gallery generation

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ function fetchData(url) {
 fetchData('https://randomuser.me/api/?results=12')
     .then(data => {
         console.log(data);
+        generateGallery(data.results);
     })
 
 /**
@@ -28,3 +29,26 @@ fetchData('https://randomuser.me/api/?results=12')
         return Promise.reject(new Error(response.statusText));
     }
 }
+
+/**
+ * Gallery
+ */
+
+function generateGallery(data) {
+    const employees = data.map(user => `
+        <div class='card'>
+            <div class='card-img-container'>
+                <img class='card-img' src='${user.picture.large}' alt='profile picture'>
+            </div>
+            <div class='card-info-container'>
+                <h3 id='name' class='card-name cap'>${user.name.first} ${user.name.last}</h3>
+                <p class='card-text'>${user.email}</p>
+                <p class='card-text cap'>${user.location.city}, ${user.location.state}</p>
+            </div>
+        </div>`).join('');
+    gallery.innerHTML = employees;
+}
+
+/**
+ * Modal
+ */
